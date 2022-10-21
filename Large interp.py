d6c8d2221971395617b7d3089423eec6e7f44ba3
ndobson <ndobson@mtu.edu>
2022-10-21 10:54:11 -0400

    continued testing of NN function

--- Large interp.py
+++ Large interp.py
@@ -5,14 +5,21 @@ import time
 # natural neighbor for fast interpolation over large datasets?
 start = time.time()
 
-num_points = 27000
+num_points = 10
+# num_points refers to the amount of points to be interpolated, the lower the number, the faster the interpolation
+# will be.
 num_dimensions = 3
 # 3D matrix, requires num_dimensions = 3
 points = np.random.rand(num_points, num_dimensions)
+# generates values within the specified matrix dimensions
 values = np.random.rand(num_points)
+# generates random points to be interpolated (will hopefully be replaced by real points in future)
 
-grid_ranges = [[0, 30, 1], [0, 30, 1], [0, 30, 1]]
+grid_ranges = [[0, 100, 1], [0, 50, 1], [0, 20, 1]]
+# defines how many values are going to be interpolated within the matrix given its size
 nn_interpolated_values = naturalneighbor.griddata(points, values, grid_ranges)
+# interpolates the matrix using natural neighbor
 print(" ", nn_interpolated_values)
-
+# prints out the interpolated values
 print("%s seconds" % (time.time() - start))
+# prints out the amount of time it took to interpolate the function
