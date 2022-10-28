2134b66c4c0966fc98ede58349806ddbf29207c3
ndobson <ndobson@mtu.edu>
2022-10-28 16:58:26 -0400

    Potential output GUI for output file location and rerunning program?

--- Output_GUI.py
+++ Output_GUI.py
@@ -2,8 +2,11 @@ import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
 
+# will need to use filedialog since the GUI would be pulling the output file location for the user
+# it's not entirely useless...I didn't say that, you did
 outgui = Tk(className="-GENEX GUI tool")
 outgui.geometry("400x400")
+
 
 def guistuff():
     outfiletxt = tk.Label(text="Location of file:")
@@ -12,7 +15,11 @@ def guistuff():
         fg="green",
         width=35,
         state='disabled'
+        # disabled to ensure the user cannot change the contents of the box, they can only copy the file path
     )
+    # TODO will need a button asking if they want to run the kriging again
+    # TODO the button will have to reopen the original GUI and restart the program for new entries
+    # TODO will also need a button asking if the user would like to quit the program
     outfiletxt.pack()
     outfile.pack()
 
