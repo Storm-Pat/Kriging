91e77d5a9ddb4c7309bc254e952afc6a13a581e0
ndobson <ndobson@mtu.edu>
2022-11-02 09:35:55 -0400

    added buttons to rerun or quit the program, need to be made functional though.

--- Output_GUI.py
+++ Output_GUI.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 from tkinter import *
-from tkinter import filedialog
+# from tkinter import filedialog
 
 # will need to use filedialog since the GUI would be pulling the output file location for the user
 # it's not entirely useless...I didn't say that, you did
 outgui = Tk(className="-GENEX GUI tool")
-outgui.geometry("400x400")
+outgui.geometry("200x200")
 
 
 def guistuff():
@@ -16,12 +16,27 @@ def guistuff():
         width=35,
         state='disabled'
         # disabled to ensure the user cannot change the contents of the box, they can only copy the file path
+        # TODO will have to input the file/file location into the entry box
     )
-    # TODO will need a button asking if they want to run the kriging again
-    # TODO the button will have to reopen the original GUI and restart the program for new entries
-    # TODO will also need a button asking if the user would like to quit the program
+
+    quitbutton = tk.Button(
+        text="QUIT",
+        bg="red",
+        fg="white",
+        width=10
+        # TODO will need to force close the program
+    )
+    runagain = tk.Button(
+        text="RUN AGAIN",
+        bg="green",
+        fg="white",
+        width=10
+        # TODO the button will have to reopen the original GUI and restart the program for new entries
+    )
     outfiletxt.pack()
     outfile.pack()
+    runagain.pack()
+    quitbutton.pack()
 
 
 guistuff()
