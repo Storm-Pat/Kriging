e56f45727000ab6b7b2a6006c5b87bf849093f8b
ndobson <ndobson@mtu.edu>
2022-11-11 11:33:29 -0500

    reformatted the large interp file

--- Large interp.py
+++ Large interp.py
@@ -1,25 +1,26 @@
 import numpy as np
-import naturalneighbor
-import time
 
-# natural neighbor for fast interpolation over large datasets?
-start = time.time()
 
-num_points = 10
-# num_points refers to the amount of points to be interpolated, the lower the number, the faster the interpolation
-# will be.
-num_dimensions = 3
-# 3D matrix, requires num_dimensions = 3
-points = np.random.rand(num_points, num_dimensions)
-# generates values within the specified matrix dimensions
-values = np.random.rand(num_points)
-# generates random points to be interpolated (will hopefully be replaced by real points in future)
+def sparse():
+    # this is part one of the matrix evaluation of the IVER3 dataset
+    X = input("How many rows: ")
+    Y = input("How many columns: ")
+    # TODO this will have to be remade to have the input be read from a file
+    X = int(X)
+    Y = int(Y)
+    A = np.random.standard_exponential(size=(X, Y))
+    A[A < 0.9] = 0
+    Asum = sum(A > 0.9)
+    B = sum(Asum)
+    C = np.sum(A)
+    print(A)
+    print("The total amount of nonzero elements in the matrix is: ", B)
+    print("The sum of all elements is: ", C)
+    D = C / B
+    print("The average value in the sparse matrix is: ", D)
+    # TODO this will have to be a loop with unknown iterations since we will have an unknown number of points
 
-grid_ranges = [[0, 100, 1], [0, 50, 1], [0, 20, 1]]
-# defines how many values are going to be interpolated within the matrix given its size
-nn_interpolated_values = naturalneighbor.griddata(points, values, grid_ranges)
-# interpolates the matrix using natural neighbor
-print(" ", nn_interpolated_values)
-# prints out the interpolated values
-print("%s seconds" % (time.time() - start))
-# prints out the amount of time it took to interpolate the function
+
+# TODO once all the sparse matrices are completed, they will need to be input into a larger grid to interpolate them
+
+sparse()
