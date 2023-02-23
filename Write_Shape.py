d835cc7023bd337e362e7305ff316246249210fb
ndobson <ndobson@mtu.edu>
2023-02-23 09:53:41 -0500

    GUI works, write shape creates correct shapefile

--- Write_Shape.py
+++ Write_Shape.py
@@ -10,6 +10,9 @@ path1 = os.path.join(path0, parent_directory)
 path2 = os.path.join(path1, directory1)
 path3 = os.path.join(path2, "output.csv")
 path4 = os.path.join(path2, "output.shp")
+path5 = os.path.join(path2, "Complete-shape.shp")
+
+
 # defining the paths
 
 
@@ -19,7 +22,7 @@ def write_file(long, lat, df):
     data = pd.DataFrame(columns)
     data.to_csv(path3)
     # writing the dataframe out to csv file
-    geodata = geopandas.GeoDataFrame(geometry=geopandas.points_from_xy(long, lat, df))
-    print(geodata)
+    geodata = geopandas.GeoDataFrame(geometry=geopandas.points_from_xy(lat, long, df))
+    # inputting the dataframe into a geodataframe
     geodata.to_file(path4, driver='ESRI Shapefile', encoding='utf-8', crs='EPSG:4326', engine="fiona")
-    # closing the shapefile
+    # inserting into a point shapefile with base coordinate system
